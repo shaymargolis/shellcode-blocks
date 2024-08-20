6a99fb367262f0ab60649ff220bd9c25ce420012
Shay Margolis <shay@inbar.co.il>
2024-08-20 15:28:57 +0300

    setup: Fix package name and include .c,.h,.S files in package

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="shellblock",
+    name="shellblocks",
     version="0.1",
     packages=find_packages(),
+    package_data={
+        "shellblocks": ["**/*.c", "**/*.S", "**/*.ld", "**/*.h"]
+    }
 )
