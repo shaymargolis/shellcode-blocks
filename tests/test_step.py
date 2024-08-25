47441f0409bd88d829088d04bdba6f6621e04a86
Shay Margolis <shay@inbar.co.il>
2024-08-25 13:31:45 +0300

    tests: Add test for ShellcodeStep

---
+++ tests/test_step.py
@@ -0,0 +1,26 @@
+import pytest
+
+from unicorn import *
+from unicorn.mips_const import *
+
+from shellblocks.shellcode_step import ShellcodeStep
+from shellblocks.primitives.memcpy import ShellcodePrimitiveMemcpy
+
+def test_step_too_large_fails(temp_dir_path):
+    step = ShellcodeStep(
+        "first_step",
+        0x1000,
+        [
+            ShellcodePrimitiveMemcpy(
+                f"copy_next_stage{i}",
+                0x2000,
+                0x3000,
+                0x10
+            )
+            for i in range(6)
+        ],
+        0x10
+    )
+
+    with pytest.raises(Exception) as e_info:
+        out_file = step.generate(temp_dir_path / step.nickname)
