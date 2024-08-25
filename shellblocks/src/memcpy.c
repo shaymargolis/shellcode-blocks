390a3ff6c5543c2167649864e61f2f78b95c5184
Shay Margolis <shay@inbar.co.il>
2024-08-25 13:31:13 +0300

    shellblocks: Use nickname as symbol name for primitives
    This enables the feature to use same primitive twice in the
    same step

--- shellblocks/src/memcpy.c
+++ shellblocks/src/memcpy.c
@@ -1,7 +1,7 @@
 #include "utils.h"
 #include "memcpy.h"
 
-void __attribute__((noreturn)) memcpy(void) {
+void __attribute__((noreturn)) start(void) {
     u8 *src = (u8 *)MEMCPY_SOURCE_ADDRESS;
     u8 *dst = (u8 *)MEMCPY_DEST_ADDRESS;
     u32 len = (u32)MEMCPY_LEN;
--- shellblocks/src/print.c
+++ shellblocks/src/print.c
@@ -1,7 +1,7 @@
 #include "utils.h"
 #include "print.h"
 
-void __attribute__((noreturn)) print(void) {
+void __attribute__((noreturn)) start(void) {
     void (*print_function)(char *) = (void (*)(char *))PRINT_FUNCTION_ADDRESS;
 
     print_function(PRINT_STRING);
