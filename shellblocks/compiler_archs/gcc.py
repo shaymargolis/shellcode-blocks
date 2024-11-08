9cd5bc67d19d1399fb23c580a58cf86fb92e0976
Shay Margolis <shay@inbar.co.il>
2024-11-08 14:32:51 +0200

    shellblocks: compiler_archs: Add gcc compiler

---
+++ shellblocks/compiler_archs/gcc.py
@@ -0,0 +1,41 @@
+from shellblocks.compiler_arch import CompilerArch
+
+
+class CompilerArchGCC(CompilerArch):
+    def __init__(self):
+        super().__init__()
+
+        self.compiler_path = self.get_compiler_path()
+
+    def get_compiler_path(self):
+        raise NotImplementedError()
+
+    def get_ldscript_path(self):
+        raise NotImplementedError()
+
+    def get_gcc_flags(self):
+        return [
+            "-nostdlib",
+            "-ffreestanding",
+        ]
+
+    def compile_primitive(self, src_path: str) -> [str]:
+        return [
+            self.compiler_path,
+            *self.get_gcc_flags(),
+            "-c", src_path,
+            "-o", "final.o",
+            "-O3"
+        ]
+
+    def compile_step(self, src_paths: [str], base_address: int) -> [str]:
+        ldscript_loc = self.get_ldscript_path()
+
+        return [
+            self.compiler_path,
+            *src_paths,
+            "-o", "shellcode.elf",
+            *self.get_gcc_flags(),
+            f"-Wl,--section-start=.text={hex(base_address)}",
+            f"-Wl,-T{ldscript_loc}"
+        ]
