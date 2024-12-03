2c091c0af4fa0c3971282520ea0e9d2f97bc2cc1
Shay Margolis <shay@inbar.co.il>
2024-12-03 13:03:20 +0200

    pritimives: Add memset primitive

---
+++ shellblocks/src/memset.c
@@ -0,0 +1,16 @@
+#include "utils.h"
+#include "memset.h"
+
+static const u8 items[] = MEMSET_ITEMS;
+
+void __attribute__((noreturn)) start(void) {
+    u8 *dst = (u8 *)MEMSET_DEST_ADDRESS;
+    u32 len = (u32)MEMSET_ITEMS_LEN;
+
+    for (int i = 0; i < MEMSET_ITEMS_LEN; i++) {
+        *dst = items[i];
+        dst++;
+    }
+
+    __builtin_unreachable();
+}
