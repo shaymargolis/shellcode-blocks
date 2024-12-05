d7898ab62bda156a38a16ffee0c0ace78784b9e3
Shay Margolis <shay@inbar.co.il>
2024-12-05 18:09:32 +0200

    arch: Add generic rel API utils

---
+++ shellblocks/src/arch/utils.h
@@ -0,0 +1,16 @@
+#ifndef SHELLCODE_BLOCKS_ARCH_UTILS_H
+#define SHELLCODE_BLOCKS_ARCH_UTILS_H
+
+#define REL_ACCESS_STRING __attribute__((section(".text")))
+
+#define GET_REL_LABEL(LABEL) "__get_" # LABEL
+
+#define DECLARE_NOP_END(var_name)                                                \
+    const u32 var_name[] REL_ACCESS_STRING = {NOP_OPCODE,NOP_OPCODE,NOP_OPCODE};
+
+#define JUMP_TO_NOP_END(var_name)              \
+    void (*var_name##_rel)();                  \
+    GET_REL_ADDRESS(var_name##_rel, var_name); \
+    var_name##_rel();
+
+#endif // !SHELLCODE_BLOCKS_ARCH_UTILS_H
