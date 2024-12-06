b485fd6e2b458a1cfc36eadba95ddf8cd53d77ca
Shay Margolis <shay@inbar.co.il>
2024-12-06 11:48:12 +0200

    tests: Add x86 and x86_64 archs

--- shellblocks/compiler_archs/__init__.py
+++ shellblocks/compiler_archs/__init__.py
@@ -1,6 +1,8 @@
 from enum import Enum
 from shellblocks.compiler_archs.mips import CompilerArchMIPSBE, CompilerArchMIPSLE
 from shellblocks.compiler_archs.arm import CompilerArchARMLE
+from shellblocks.compiler_archs.x86 import CompilerArchX86
+from shellblocks.compiler_archs.x86_64 import CompilerArchX86_64
 from shellblocks.compiler_arch import CompilerArch
 
 
@@ -8,6 +10,8 @@ class CompilerArchOption(Enum):
     MIPSBE = "mipsbe"
     MIPSLE = "mipsle"
     ARMLE = "armle"
+    X86 = "x86"
+    X86_64 = "x86_64"
 
 
 def compiler_arch_to_object(arch: CompilerArchOption) -> CompilerArch:
@@ -17,6 +21,10 @@ def compiler_arch_to_object(arch: CompilerArchOption) -> CompilerArch:
         return CompilerArchMIPSLE()
     elif arch == CompilerArchOption.ARMLE:
         return CompilerArchARMLE()
+    elif arch == CompilerArchOption.X86:
+        return CompilerArchX86()
+    elif arch == CompilerArchOption.X86_64:
+        return CompilerArchX86_64()
 
     raise NotImplementedError()
 
